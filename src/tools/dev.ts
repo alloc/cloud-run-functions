39c404268b1567eb837d2d1f723df43b706f73c1
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-05-26 14:02:58 -0400

    feat(dev): accept custom `port` and child process options

--- src/tools/dev.ts
+++ src/tools/dev.ts
@@ -1,11 +1,15 @@
 import { findUpSync } from 'find-up-simple'
 import path from 'node:path'
-import $ from 'picospawn'
+import $, { PicospawnOptions } from 'picospawn'
+
+export type DevOptions = PicospawnOptions & {
+  port?: string | number
+}
 
 /**
  * Start the development server in a separate process.
  */
-export function dev(root?: string) {
+export function dev(root?: string, { port, ...options }: DevOptions = {}) {
   const packageDir = findUpSync('dist', {
     cwd: import.meta.dirname,
     type: 'directory',
@@ -14,15 +18,20 @@ export function dev(root?: string) {
   const source = path.join(packageDir, 'targets/dev.js')
   const binDir = path.resolve(packageDir, '../node_modules/.bin')
 
-  return $('functions-framework --target=dev --source', [source], {
-    stdio: 'inherit',
-    env: {
-      ...(options.env ?? process.env),
-      CRF_OPTIONS: JSON.stringify({
-        searchDir: root,
-        workingDir: process.cwd(),
-      }),
-      PATH: `${binDir}:${process.env.PATH}`,
-    },
-  })
+  return $(
+    'functions-framework --target=dev --source %s',
+    [source, port != null && ['--port', port.toString()]],
+    {
+      stdio: 'inherit',
+      ...options,
+      env: {
+        ...(options.env ?? process.env),
+        CRF_OPTIONS: JSON.stringify({
+          searchDir: root,
+          workingDir: process.cwd(),
+        }),
+        PATH: `${binDir}:${process.env.PATH}`,
+      },
+    }
+  )
 }
