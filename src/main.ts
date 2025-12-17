21ed2b3a033d2dc45c7b6d7e30bb082122fadfcc
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-12-16 21:22:30 -0500

    chore(refactor): drop ordana for cmd-ts

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,56 @@
-import * as ordana from 'ordana'
+import {
+  command,
+  option,
+  optional,
+  positional,
+  run,
+  string,
+  subcommands,
+} from 'cmd-ts'
 
-const command = ordana.parse(process.argv.slice(2), {
-  name: 'cloud-run-functions',
-  subcommands: {
-    dev: {
-      description: 'Start the development server',
-      positionals: { maximum: 1 },
-    },
-    build: {
-      description: 'Generate a bundle for each function',
-      positionals: { maximum: 1 },
-    },
+const dev = command({
+  name: 'dev',
+  description: 'Start the development server',
+  args: {
+    root: positional({
+      type: optional(string),
+      displayName: 'root',
+      description: 'Directory to search for function entrypoints',
+    }),
+    port: option({
+      type: optional(string),
+      long: 'port',
+      short: 'p',
+      description: 'The port to use for the development server',
+    }),
+  },
+  async handler({ root, port }) {
+    const { dev } = await import('./tools/dev')
+    await dev(root, { port })
   },
 })
 
-if (command.type === 'help') {
-  console.log(ordana.generateHelpMessage(command))
-} else {
-  const subcommands = {
-    async dev() {
-      const { dev } = await import('./tools/dev')
-      await dev(command.positionals[0])
-    },
-    async build() {
-      throw new Error('Not implemented')
-    },
-  }
-  await subcommands[command.subcommand]()
-}
+const build = command({
+  name: 'build',
+  description: 'Generate a bundle for each function',
+  args: {
+    root: positional({
+      type: optional(string),
+      displayName: 'root',
+      description: 'Directory to search for function entrypoints',
+    }),
+  },
+  async handler() {
+    throw new Error('Not implemented')
+  },
+})
+
+const cli = subcommands({
+  name: 'cloud-run-functions',
+  cmds: {
+    dev,
+    build,
+  },
+})
+
+await run(cli, process.argv.slice(2))
