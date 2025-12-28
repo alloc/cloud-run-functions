2feea23f6bd1503b052344fd6d2a7a38501599a3
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-12-27 20:12:41 -0500

    feat: add --define option to dev command

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import {
+  array,
   command,
+  multioption,
   option,
   optional,
   positional,
@@ -7,6 +9,7 @@ import {
   string,
   subcommands,
 } from 'cmd-ts'
+import { objectify } from 'radashi'
 
 const dev = command({
   name: 'dev',
@@ -23,10 +26,30 @@ const dev = command({
       short: 'p',
       description: 'The port to use for the development server',
     }),
+    define: multioption({
+      type: optional(array(string)),
+      long: 'define',
+      short: 'd',
+      description: 'Statically replace specific variables in the source code',
+    }),
   },
-  async handler({ root, port }) {
+  async handler({ root, port, define }) {
     const { dev } = await import('./tools/dev')
-    await dev(root, { port })
+    await dev(root, {
+      port,
+      define:
+        define &&
+        objectify(
+          define.map(d => d.split(':') as [string, string?]),
+          ([key]) => key,
+          ([, value]) =>
+            value
+              ? isNaN(parseFloat(value))
+                ? JSON.stringify(value)
+                : value
+              : ''
+        ),
+    })
   },
 })
 
